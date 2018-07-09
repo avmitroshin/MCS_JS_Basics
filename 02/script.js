8951cbdfd8d0557d66b35b71a51e782184146cd7
avmitroshin <40958031+avmitroshin@users.noreply.github.com>
2018-07-10 00:37:47 +0700

    commit practice 3.9

---
+++ 02/script.js
@@ -0,0 +1,6 @@
+document.body.style.backgroundColor =  prompt("Какой сделать цвет страницы?");
+document.getElementsByClassName("page")[0].style.color = prompt("Какой сделать цвет текста?");
+document.querySelector("span.name").textContent = prompt("Имя?");
+document.getElementsByTagName("img")[0].setAttribute("src",prompt("Введите адрес картинки"));
+document.getElementsByClassName("shortBio")[0].textContent =  prompt("Введите текст страницы");
+document.getElementsByClassName("shortBio")[0].setAttribute("class",this.getAttribute("class")+" animated");
