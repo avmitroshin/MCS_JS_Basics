d41c2f54a289cfc513a6bd92e9c0fad205c83412
avmitroshin <40958031+avmitroshin@users.noreply.github.com>
2018-07-10 00:52:33 +0700

    commit practice 3.9 2

--- 02/script.js
+++ 02/script.js
@@ -3,4 +3,4 @@ document.getElementsByClassName("page")[0].style.color = prompt("Какой сд
 document.querySelector("span.name").textContent = prompt("Имя?");
 document.getElementsByTagName("img")[0].setAttribute("src",prompt("Введите адрес картинки"));
 document.getElementsByClassName("shortBio")[0].textContent =  prompt("Введите текст страницы");
-document.getElementsByClassName("shortBio")[0].setAttribute("class",this.getAttribute("class")+" animated");
+document.getElementsByClassName("shortBio")[0].setAttribute("class",document.getElementsByClassName("shortBio")[0].getAttribute("class")+" animated");
