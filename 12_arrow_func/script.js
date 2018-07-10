e6cd8475393d8a7e633b85be1178f91c559b3e50
avmitroshin <40958031+avmitroshin@users.noreply.github.com>
2018-07-10 12:15:50 +0700

    Update script.js

--- 12_arrow_func/script.js
+++ 12_arrow_func/script.js
@@ -1,12 +1,11 @@
 let pocket = Number.parseFloat(prompt("Сколько у вас денег?"));
-let ApplesQty = Number.parseInt(prompt("Сколько куплено яблок?"));
-let BreadQty = Number.parseInt(prompt("Сколько куплено батонов хлеба?"));
-let ApplesPrc = Number.parseFloat(prompt("Сколько стоит яблоко?"));
-let BreadPrc = Number.parseFloat(prompt("Сколько стоит батон?"));
-let result = pocket >= (ApplesQty*ApplesPrc + BreadQty*BreadPrc);
+let applesQty = Number.parseInt(prompt("Сколько куплено яблок?"));
+let breadQty = Number.parseInt(prompt("Сколько куплено батонов хлеба?"));
+let applesPrc = Number.parseFloat(prompt("Сколько стоит яблоко?"));
+let breadPrc = Number.parseFloat(prompt("Сколько стоит батон?"));
 
-function haveEnough(pocket, applsQty,breadQty,applesPrc,breadPrc){
-  return pocket >= (ApplesQty*ApplesPrc + BreadQty*BreadPrc) ? "Вам хватает денег" : "Вам НЕ хватает денег";
+function haveEnough(pocket, applesQty,breadQty,applesPrc,breadPrc){
+  return pocket >= (applesQty*applesPrc + breadQty*breadPrc) ? "Вам хватает денег" : "Вам НЕ хватает денег";
 }
 
-console.log(haveEnough(100,5,3,15,10));
+console.log(haveEnough(pocket,applesQty,breadQty,applesPrc,breadPrc));
